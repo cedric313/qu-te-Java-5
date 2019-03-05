29bfaf8c5238acc20d613187da84615dbfa74c63
cedric <casas.cedric@gmail.com>
2019-03-05 20:50:24 +0100

    Movies.java

---
+++ Movies.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class Movies {
+
+	public static void main (String[] args) {
+
+	String[] moviesIndiana = {"le temple maudit" , "la derniére croisade" , "les aventuriers de l'arche perdue"};
+	String[] templeMaudit = {"Harisson Ford" , "Kate Capshaw", "Jonathan Ke Quan"};
+	String[] derniereCroisade = {"Harrison Ford" , "Sean Connery" , "Denholm Elliot"};
+	String[] aventuriersArchePerdue = {"Harrison Ford", "Karen Allen", "Paul Freeman"};	
+
+
+
+	String[][] movies;
+	movies = new String[][] {
+
+	moviesIndiana,
+	templeMaudit,
+	derniereCroisade,
+	aventuriersArchePerdue
+
+
+
+	};
+
+	System.out.println();
+
+
+	}
+
+
+
+
+
+
+}
